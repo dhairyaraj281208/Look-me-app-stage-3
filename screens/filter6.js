c9ffeeee41fe62486b23615dbe69ce7cdc8af252
dhairyaraj281208 <72195034+dhairyaraj281208@users.noreply.github.com>
2021-11-14 15:17:39 +0530

    Add files via upload

---
+++ screens/filter6.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Image, View } from 'react-native';
+
+const Filter6 = ({
+    face: {
+        bounds: {
+            size: { width: faceWidth, height: faceHeight }
+        },
+        leftEyePosition,
+        rightEyePosition,
+        noseBasePosition
+    }
+}) => {
+    const filtersWidth = faceWidth 
+    const filtersHeight = faceHeight / 3
+
+    const transformAngle = (
+        angleRad = Math.atan(
+            (rightEyePosition.y - leftEyePosition.y) /
+            (rightEyePosition.x - leftEyePosition.x)
+        )
+    ) => angleRad * 180 / Math.PI
+    
+    return (
+        <View style={{
+            position: 'absolute',
+            left: leftEyePosition.x - filtersWidth * 0.675,
+            top: leftEyePosition.y - filtersHeight * 0.5 - 120
+        }}>
+            <Image
+                source={require('../assets/hat-pic2.png')}
+                style={{
+                    width: filtersWidth,
+                    height: filtersHeight,
+                    resizeMode: 'contain',
+                    transform: [{ rotate: `${transformAngle()}deg` }]
+                }}
+            />
+        </View>
+    );
+};
+
+export default Filter6
